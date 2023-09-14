723219ceb0ee6d00ef0b972af27f3caff881ffd0
Igor <igor.tub18@gmail.com>
2023-09-13 21:08:20 -0300

    feat: permite acessar o id do vídeo no componente principal.

--- src/components/video-input-form.tsx
+++ src/components/video-input-form.tsx
@@ -17,7 +17,11 @@ const statusMessages = {
   success: 'Sucesso!'
 }
 
-export function VideoInputForm() {
+interface VideoInputFormProps {
+  onVideoUploaded: (videoId: string) => void
+}
+
+export function VideoInputForm(props: VideoInputFormProps) {
     const [videoFile, setVideoFile] = useState<File | null>(null)
     const promptInputRef = useRef<HTMLTextAreaElement>(null)
     const [status, setStatus] = useState<Status>('waiting')
@@ -101,6 +105,8 @@ export function VideoInputForm() {
       await api.post(`/videos/${videoId}/transcription`, {prompt})
 
       setStatus('success')
+
+      props.onVideoUploaded(videoId)
     }
 
     const previewURL = useMemo(() => {
